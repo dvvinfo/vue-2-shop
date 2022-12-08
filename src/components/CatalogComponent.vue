a2e9d8f61cc3936150f971781c08fd45131de607
dvvinfo <dvvinfo@mail.ru>
2022-12-08 12:39:30 +0300

    added receiving data from the server

--- src/components/CatalogComponent.vue
+++ src/components/CatalogComponent.vue
@@ -14,69 +14,30 @@
 
 <script>
 import CatalogItemComponent from "./CatalogItemComponent.vue";
+import { mapActions, mapGetters } from "vuex";
 export default {
   name: "CatalogComponent",
   components: {
     CatalogItemComponent,
   },
   data() {
-    return {
-      products: [
-        {
-          image: "1.jpg",
-          name: "T-shirt 1",
-          price: 100,
-          article: "T1",
-          available: true,
-          category: "Мужские",
-        },
-        {
-          image: "2.jpg",
-          name: "T-shirt 2",
-          price: 150,
-          article: "T2",
-          available: true,
-          category: "Женские",
-        },
-        {
-          image: "3.jpg",
-          name: "T-shirt 3",
-          price: 200,
-          article: "T3",
-          available: false,
-          category: "Женские",
-        },
-        {
-          image: "4.jpg",
-          name: "T-shirt 4",
-          price: 300,
-          article: "T4",
-          available: true,
-          category: "Мужские",
-        },
-        {
-          image: "5.jpg",
-          name: "T-shirt 5",
-          price: 500,
-          article: "T5",
-          available: false,
-          category: "Женские",
-        },
-        {
-          image: "6.jpeg",
-          name: "T-shirt 6",
-          price: 700,
-          article: "T6",
-          available: true,
-          category: "Женские",
-        },
-      ],
-    };
+    return {};
+  },
+  computed: {
+    ...mapGetters(["products"]),
   },
   methods: {
+    ...mapActions(["getProductsFromApi"]),
     showArticle(data) {
       console.log(data);
     },
+  },
+  mounted() {
+    this.getProductsFromApi().then((response) => {
+      if (response.data) {
+        console.log("Data arrived!");
+      }
+    });
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,37 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    products: [],
+  },
+  getters: {
+    products: (state) => {
+      return state.products;
+    },
+  },
+  mutations: {
+    setProductsToStart(state, products) {
+      state.products = products;
+    },
+  },
+  actions: {
+    getProductsFromApi({ commit }) {
+      return axios("http://localhost:3000/products", {
+        method: "GET",
+      })
+        .then((products) => {
+          commit("setProductsToStart", products.data);
+          return products;
+        })
+        .catch((error) => {
+          console.log(error);
+          return error;
+        });
+    },
+  },
   modules: {},
 });
